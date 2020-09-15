01a30fe8665a39b680f46b875b5860c2299033fa
Joy Evans <joyevans@me.com>
2020-09-15 18:05:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,11 +8,11 @@ end
 
 introduction_with_language("Joy", "Ruby")
 
-def introduction_with_language(name, language = "Ruby")
+def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-
+introduction_with_language_optional("Joy")
 
 
 
